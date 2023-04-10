7c35bbf5221e6e53c60c732f6cd55401ce5428f7
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-10 11:45:24 +0300

    Выполнение

--- 3.1 Class/Task 3.1 Class.py
+++ 3.1 Class/Task 3.1 Class.py
@@ -42,3 +42,34 @@ c1 = MyTable()
 print(c1.get_table(1, 12))
 c2 = MyTable()
 print(c2.get_table(3, 9))
+
+# Хех, хитро!)
+# Ну, это не то что я ожидал, если честно) объект DataFrame и есть таблица из модудя pandas
+# а так вариант например такой
+
+class Table:
+    
+    def __init__(self,rows, cols):
+        self.field = [ [None for _ in range(cols)] for _ in range(rows) ]
+        self.rows = rows
+        self.cols = cols
+        
+    def get_value(self, row, col):
+        if  row in range(self.rows) and col in range(self.cols):
+            return self.field[row][col]
+        return
+ 
+    def set_value(self, row, col,value):
+        self.field[row][col] = value
+    
+    def n_rows(self):
+        return self.rows
+ 
+    def n_cols(self):
+        return self.cols
+
+matrix = Table(rows=10, cols=10)
+print(matrix.n_cols())
+print(matrix.get_value(2, 2))
+matrix.set_value(2, 2, 'a')
+print(matrix.get_value(2, 2))
